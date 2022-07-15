449a60f917d961b35cd01e86804a6f05fa40aa90
Zak Mouchtati <zakaria.mouchtati01@gmail.com>
2022-07-15 11:03:47 +0100

    first commit

---
+++ src/components/HelloWord.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Home.css'
+const HelloWord = () => {
+  return (
+    <div>
+        <h1>
+            Hello Word Form React Component Jsx
+      
+        </h1>
+    </div>
+  )
+}
+
+export default HelloWord
